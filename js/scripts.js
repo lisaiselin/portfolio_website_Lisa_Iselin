60e6070ef09fe5de2f0f8b63711bed5814e7a556
lisaiselin <lisaiselin@gmail.com>
2021-04-19 12:52:15 -0600

    Created and linked js file
    • Created js file
    • Linked js file to all html files
    • Added message to js file

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+console.log("Hi! Welcome to my portfolio site.");
+
+//This is a single line comment, started with two slashes
+
+/* This is a multi-line recommendationsEverything in here has no effect.
+var test = 'This will not actually create a variable';
+*/
+
+//Everything after the slashes has no effect
