05991f135d2b5e15668fa294a5a09f3e56990aaa
lisaiselin <lisaiselin@gmail.com>
2021-05-13 14:47:35 -0600

    UPdates
    Code quality
    Browser testing
    Device testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
-function menuToggle(){
-  var x = document.getElementById ('myNavtoggle');
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
 
-//This is a single line comment, started with two slashes
+// This is a single line comment, started with two slashes
 
 /* This is a multi-line recommendationsEverything in here has no effect.
 var test = 'This will not actually create a variable';
 */
 
-//Everything after the slashes has no effect
+// Everything after the slashes has no effect
